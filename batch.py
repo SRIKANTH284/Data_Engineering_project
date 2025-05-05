a5ad929d404e251d1f7c3bb4f03fc10d0503675c
Srikanth <srikanth@Srikanths-MacBook-Pro.local>
2025-05-05 09:26:59 -0400

    update

--- batch.py
+++ batch.py
@@ -1,13 +1,19 @@
+# Import SparkSession from PySpark
 from pyspark.sql import SparkSession
 
 # Initialize Spark session and context
 spark = SparkSession.builder.appName("ExampleRDD").getOrCreate()
+#work with RDDs
 sc = spark.sparkContext
 
-# Read CSV as RDD
+# Read input CSV as raw text RDD(each line is string)
 rdd = sc.textFile("batch_input/data.csv")
 
+#apply series of transformation
 # Split by comma, filter and map
+#filter rows where 3rd column index2 >50
+#create (k,1) pairs using 2nd column index 1 - counting
+# reduce by key to count total occurrences per key
 rdd_filtered = rdd.map(lambda line: line.split(",")) \
                             .filter(lambda fields: int(fields[2]) > 50) \
                             .map(lambda fields: (fields[1], 1)) .reduceByKey(lambda x, y: x + y)
